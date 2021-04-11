7ec561ce183179cc42edee38455149e0c374b702
Ben-Cheng <BCheng@linz.govt.nz>
2021-04-12 09:51:57 +1200

    test: added test for trace level

--- linz_logger/test_logger.py
+++ linz_logger/test_logger.py
@@ -2,9 +2,18 @@
 Tests for the hello() function.
 """
 
+from structlog.testing import capture_logs
+
 from .logger import get_log
 
 
 def test_hello_without_name():
     """Test with no parameter."""
     assert get_log() is not None
+
+
+def test_trace():
+    with capture_logs() as cap_logs:
+        get_log().trace("abc")
+        assert cap_logs[0]["event"] == "abc"
+        assert cap_logs[0]["log_level"] == "trace"
